5510b5f43de8357ad7f8ac9a03e67d4370b0a7b4
shaon-chowdhury-euc <shaon.chowdhury@eucalyptus.vc>
2025-10-24 13:09:44 +1100

    fixing alembic revisions

--- mlflow/store/db_migrations/versions/xyz123abc456_fix_model_version_type_varchar.py
+++ mlflow/store/db_migrations/versions/xyz123abc456_fix_model_version_type_varchar.py
@@ -1,6 +1,6 @@
-"""fix model_version column type from integer to varchar
+"""fix model_version type to varchar
 
-Create Date: 2025-10-24 01:00:00.000000
+Create Date: 2025-10-24 03:30:00.000000
 
 """
 
@@ -8,7 +8,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = "abc123def456"
+revision = "xyz123abc456"
 down_revision = "bf29a5ff90ea"
 branch_labels = None
 depends_on = None
@@ -18,8 +18,8 @@ def upgrade():
     """
     Fix model_versions.version and model_version_tags.version column types.
     
-    MLflow queries these columns as VARCHAR but they were created as INTEGER,
-    causing "operator does not exist: integer = character varying" errors.
+    MLflow 3.5.1 queries these columns as VARCHAR but they were created as INTEGER.
+    This causes "operator does not exist: integer = character varying" errors.
     """
     # Drop foreign key constraint
     op.drop_constraint(
@@ -60,14 +60,14 @@ def downgrade():
     """
     Revert to INTEGER type (not recommended, will break queries).
     """
-    # Drop foreign key constraint
+    # Drop foreign key
     op.drop_constraint(
         "model_version_tags_name_version_fkey",
         "model_version_tags",
         type_="foreignkey"
     )
     
-    # Revert column types to INTEGER
+    # Revert columns to INTEGER
     op.alter_column(
         "model_versions",
         "version",
@@ -84,7 +84,7 @@ def downgrade():
         existing_nullable=False
     )
     
-    # Re-create foreign key constraint
+    # Re-create foreign key
     op.create_foreign_key(
         "model_version_tags_name_version_fkey",
         "model_version_tags",
